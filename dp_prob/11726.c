eac1020178401ea8a9b0ab6f9bdc41046e87ccba
Taesueng Lee <taeseunglee159@gmail.com>
2018-03-20 23:36:40 +0900

    solve problems and change file extension from cpp to c

---
+++ dp_prob/11726.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define DIV 10007
+int dp[1001];
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+
+  dp[0] = 1;
+  dp[1] = 1;
+  for (int i = 2; i <= n; ++i) {
+    dp[i] = (dp[i-1] + dp[i-2]) % DIV;
+  }
+
+  printf("%d\n", dp[n]);
+
+  return 0;
+}
